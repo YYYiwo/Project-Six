abc7e09813bed5e00a9fcdadf78d145eb93a911f
wz <531833765@qq.com>
2020-11-16 17:01:14 +0800

    test

--- src/main/java/com/project/demo/ZbwTest.java
+++ src/main/java/com/project/demo/ZbwTest.java
@@ -2,6 +2,6 @@ package com.project.demo;
 
 public class ZbwTest {
     public static void main(String[] args) {
-        System.out.println("这是一条测试代码");
+        System.out.println("Test");
     }
 }
